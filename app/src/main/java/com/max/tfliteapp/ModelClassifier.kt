a9d9739e719e5bec577ecd9951f27c99119264d7
Navneet <navneetkhar24@gmail.com>
2025-06-07 20:55:21 +0530

    Adding Hilt dependencies

---
+++ app/src/main/java/com/max/tfliteapp/ModelClassifier.kt
@@ -0,0 +1,7 @@
+package com.max.tfliteapp
+
+import javax.inject.Inject
+
+class ModelClassifier @Inject constructor() {
+
+}
---
+++ app/src/main/java/com/max/tfliteapp/viewModel/MainActivityViewModel.kt
@@ -0,0 +1,7 @@
+package com.max.tfliteapp.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+
+}
